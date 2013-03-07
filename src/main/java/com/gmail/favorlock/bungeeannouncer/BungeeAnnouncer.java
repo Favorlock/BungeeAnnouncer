a677ab2e5cc3de8d659420a24868d4946755024e
Favorlock <favorlock@gmail.com>
2013-03-06 23:39:14 -0800

    Added 3 Commands - announce_add - announce_remove - announce_list

--- src/main/java/com/gmail/favorlock/bungeeannouncer/BungeeAnnouncer.java
+++ src/main/java/com/gmail/favorlock/bungeeannouncer/BungeeAnnouncer.java
@@ -1,9 +1,11 @@
 package com.gmail.favorlock.bungeeannouncer;
 
 import java.util.Timer;
-import java.util.TimerTask;
 import java.util.logging.Level;
 
+import com.gmail.favorlock.bungeeannouncer.cmd.AnnounceAdd;
+import com.gmail.favorlock.bungeeannouncer.cmd.AnnounceList;
+import com.gmail.favorlock.bungeeannouncer.cmd.AnnounceRemove;
 import com.gmail.favorlock.bungeeannouncer.task.AnnounceTask;
 
 import net.md_5.bungee.api.ProxyServer;
@@ -12,17 +14,21 @@ import net.md_5.bungee.api.plugin.Plugin;
 public class BungeeAnnouncer extends Plugin {
 	
 	private MainConfig config;
+	private AnnounceTask task;
 	private Timer timer = new Timer();
 	
 	public void onEnable() {
 		
-		 try {
+		try {
 			 config = new MainConfig(this); // create config
 			 config.init(); // load config file if it exists, create it if it doesn't
 		} catch(Exception ex) {
 			 ProxyServer.getInstance().getLogger().log(Level.SEVERE, "FAILED TO LOAD CONFIG!!!", ex);
 			 return;
 		}
+		
+		// Register Plugin Commands
+		registerCommands();
 		
 		// Schedule Announcement Timer
 		sendAnnouncement();
@@ -34,12 +40,22 @@ public class BungeeAnnouncer extends Plugin {
 	}
 	
 	public void sendAnnouncement() {
-		TimerTask task = new AnnounceTask(this);
+		task = new AnnounceTask(this);
 		timer.schedule(task, 0, this.getConfigStorage().settings_interval * 1000);
 	}
 	
 	public MainConfig getConfigStorage() {
 		return config;
+	}
+	
+	public void registerCommands() {
+		ProxyServer.getInstance().getPluginManager().registerCommand(new AnnounceAdd(this));
+		ProxyServer.getInstance().getPluginManager().registerCommand(new AnnounceRemove(this));
+		ProxyServer.getInstance().getPluginManager().registerCommand(new AnnounceList(this));
+	}
+	
+	public AnnounceTask getAnnounceTask() {
+		return task;
 	}
 
 }
--- src/main/java/com/gmail/favorlock/bungeeannouncer/MainConfig.java
+++ src/main/java/com/gmail/favorlock/bungeeannouncer/MainConfig.java
@@ -2,12 +2,19 @@ package com.gmail.favorlock.bungeeannouncer;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.logging.Level;
+
+import com.gmail.favorlock.bungeeannouncer.utils.FontFormat;
+
+import net.craftminecraft.bungee.bungeeyaml.InvalidConfigurationException;
 import net.craftminecraft.bungee.bungeeyaml.supereasyconfig.Config;
-import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
 
 public class MainConfig extends Config {
 	
-	public MainConfig(Plugin plugin) {
+	public MainConfig(BungeeAnnouncer plugin) {
 		CONFIG_FILE = new File("plugins" + File.separator + plugin.getDescription().getName(), "config.yml");
 		CONFIG_HEADER = "BungeeAnnouncer - Global Server Announcments";
 	}
@@ -19,5 +26,52 @@ public class MainConfig extends Config {
 		add("&4Enjoy your stay!");
 		add("&4Vote for &amoney!");
 	}};
+	
+	public void addAnnouncement(String announcement) {
+		announcements_global.add(announcement);
+		try {
+			this.save();
+		} catch (InvalidConfigurationException e) {
+			ProxyServer.getInstance().getLogger().log(Level.SEVERE, "Failed to save the config!", e);
+		}
+		
+	}
+	
+	public void removeAnnouncement(CommandSender sender, Integer id) {
+		if (id > this.announcements_global.size() - 1 || id < 0) {
+			sender.sendMessage(FontFormat.translateString("&7No announcement exist with id: &a" + id));
+			return;
+		}
+		ListIterator<String> listIterator = this.announcements_global.listIterator();
+		Integer counter = 0;
+		
+		listIterator.next();
+		
+		if (this.announcements_global.size() > 1) {
+			for (String announcement : this.announcements_global) {
+				if (counter == id) {
+					listIterator.remove();
+					try {
+						this.save();
+						sender.sendMessage(FontFormat.translateString("&aThe announcement has been deleted"));
+					} catch (InvalidConfigurationException e) {
+						ProxyServer.getInstance().getLogger().log(Level.SEVERE, "Failed to save the config!", e);
+					}
+					return;
+				} else {
+					listIterator.next();
+					counter++;
+				}
+			}
+		} else {
+			this.announcements_global = new ArrayList<String>();
+			try {
+				this.save();
+				sender.sendMessage(FontFormat.translateString("&aThe announcement has been deleted"));
+			} catch (InvalidConfigurationException e) {
+				ProxyServer.getInstance().getLogger().log(Level.SEVERE, "Failed to save the config!", e);
+			}
+		}
+	}
 
 }
